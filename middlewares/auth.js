148da2461ae2b5aac15eb1d5f4d0a652670b1ddb
focus110 <iamemmaochuko@gmail.com>
2022-05-05 01:43:17 +0100

    first commit

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+const res = require("express/lib/response");
+
+const jwt = required("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split("")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, "Snippet_SceretKEY", {
+    expireIn: "1h",
+  });
+}
+
+module.exports = {
+    authenticateToken,
+    generateAccessToken,
+}
